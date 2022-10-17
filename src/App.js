78c270c50392ac06f3218e14bc26ccb01c94edbe
bkmahapatra <bkmahapatra27@gmail.com>
2022-10-17 15:55:44 +0530

    searchbar complete

--- src/App.js
+++ src/App.js
@@ -1,15 +1,65 @@
-import './App.css';
-import Navbar from './components/Navbar';
-import Home from './components/Home';
+import "./App.css";
+import { useState, useEffect } from "react";
+import Navbar from "./components/Navbar";
+import axios from "axios";
+import MoiveItem from "./components/MoiveItem";
 
 function App() {
-  return (
-      <div id="main">
+  const [movies, setMovies] = useState([]);
+  const [searchData, setSearchData] = useState([]);
+
+  const updateData = async () => {
+    const url =
+      "https://test-apis.veeksha.app/ba6evgdsa/tywcavgshdrfjdgvc.json";
+    const response = await axios.get(url);
+    const data = response.data;
+    setMovies(data);
+    console.log(movies);
+  };
+
+  useEffect(() => {
+    updateData();
+  }, []);
+
+  const searchFunc = (words) => {
+    if (words.trim().length > 0) {
       
-      <Navbar/>
-      <Home />
+      const posts = movies.filter((item) =>
+        Object.values(item).join(" ").toLocaleLowerCase().includes(words)
+      );
+      console.log("high");
+      setSearchData(posts);
+    }
+  };
+  return (
+    <div id="main">
+      <Navbar searchKey={searchFunc} />
+      <div className="movie_posts" >
+        {movies.length > 0 && searchData.length > 0
+          ? searchData.map((item) => {
+              return (
+                <MoiveItem 
+                  key={item.imdbID}
+                  mv_title={item.Title}
+                  mv_poster={item.Poster}
+                  mv_year={item.Year}
+                  mv_type={item.Type}
+                />
+              );
+            })
+          : movies.map((item) => {
+              return (
+                <MoiveItem
+                  key={item.imdbID}
+                  mv_title={item.Title}
+                  mv_poster={item.Poster}
+                  mv_year={item.Year}
+                  mv_type={item.Type}
+                />
+              );
+            })}
       </div>
-    
+    </div>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,25 @@
-import React from 'react';
-import '../stylesheets/Navbar.css';
+import React,{useRef} from "react";
+import "../stylesheets/Navbar.css";
 
-function Navbar() {
+function Navbar({searchKey}) {
+  const inputData=useRef();
+
+  const searchData=()=>{
+    //console.log(inputData.current.value);
+   searchKey(inputData.current.value.toLowerCase())
+  }
+
   return (
     <>
-        
+      <div className="navbar">
+        <a href="/">MovieMania</a>
+        <div>
+          <input ref={inputData} onChange={searchData} className="search_bar" type="text" name="" id="search" placeholder="Search Here"/>
+          {/*   <button className="search_bar" >Go</button> */}
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
 export default Navbar;
